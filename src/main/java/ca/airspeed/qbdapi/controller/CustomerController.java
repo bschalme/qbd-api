0c9cceedbce386de5d3af9f1e80c5b7cc917ac72
Brian Schalme <bschalme@airspeed.ca>
2019-12-23 15:07:54 -0600

    Use MySQL instead of H2 in Production.

--- src/main/java/ca/airspeed/qbdapi/controller/CustomerController.java
+++ src/main/java/ca/airspeed/qbdapi/controller/CustomerController.java
@@ -5,11 +5,14 @@ import static java.lang.String.format;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import ca.airspeed.qbdapi.domain.Customer;
 import ca.airspeed.qbdapi.repository.CustomerRepository;
 import ca.airspeed.qbdapi.resource.AllCustomersResource;
 import ca.airspeed.qbdapi.resource.CustomerResource;
+import io.micronaut.data.model.Page;
+import io.micronaut.data.model.Pageable;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.hateoas.Resource;
@@ -27,17 +30,32 @@ public class CustomerController {
     }
 
     @Get("/")
-    public AllCustomersResource findAllCustomers() {
+    public Page<CustomerResource> findAllCustomers(Pageable pageable) {
         log.debug("Received a request for findAllCustomers().");
-        List<Resource> resources = new ArrayList<>();
-        for (Customer customer: repo.findAll()) {
+        List<CustomerResource> resources = new ArrayList<>();
+        for (Customer customer: repo.findAll(pageable)) {
             CustomerResource resource = new CustomerResource(customer);
             resource.link(SELF, format("/customers/%s", customer.getListID()));
             resources.add(resource);
         }
-        AllCustomersResource allCustomers = new AllCustomersResource();
-        allCustomers.link(SELF, "/customers");
-        allCustomers.embedded("customers", resources);
-        return allCustomers;
+//        AllCustomersResource allCustomers = new AllCustomersResource();
+//        allCustomers.link(SELF, "/customers");
+//        allCustomers.embedded("customers", resources);
+        return Page.of(resources, pageable, 3);
+//        return allCustomers;
+    }
+
+    @Get("/{customerId}")
+    public CustomerResource findOneCustomer(String customerId) {
+        log.debug("Received a request for findOneCustomer().");
+        Optional<Customer> result = repo.findById(customerId);
+        if (result.isPresent()) {
+            CustomerResource resource = new CustomerResource(result.get());
+            resource.link(SELF, format("/customers/%s", result.get().getListID()));
+            return resource;
+        }
+        else {
+            return null;
+        }
     }
 }
