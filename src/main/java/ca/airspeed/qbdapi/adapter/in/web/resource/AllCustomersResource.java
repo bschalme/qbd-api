d9eb27d1097fd80e65ab9d7060c96f488bd85693
Brian Schalme <bschalme@airspeed.ca>
2020-09-08 20:04:04 -0500

    Tune the JaCoCo test coverage report. Removed some dead code.

--- src/main/java/ca/airspeed/qbdapi/adapter/in/web/resource/AllCustomersResource.java
+++
@@ -1,7 +0,0 @@
-package ca.airspeed.qbdapi.adapter.in.web.resource;
-
-import io.micronaut.http.hateoas.AbstractResource;
-
-public class AllCustomersResource extends AbstractResource<AllCustomersResource> {
-
-}
