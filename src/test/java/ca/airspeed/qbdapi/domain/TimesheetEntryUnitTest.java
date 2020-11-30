a59ce52d05c1be715b0b4ef80525a3c10364b634
Brian Schalme <bschalme@airspeed.ca>
2020-11-29 22:00:17 -0600

    Bump Gradle from 6.7 to 6.7.1. Make a validation test class more maintainable.

--- src/test/java/ca/airspeed/qbdapi/domain/TimesheetEntryUnitTest.java
+++ src/test/java/ca/airspeed/qbdapi/domain/TimesheetEntryUnitTest.java
@@ -11,6 +11,8 @@ import javax.inject.Inject;
 import javax.validation.ConstraintViolation;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import io.micronaut.validation.validator.Validator;
@@ -34,11 +36,16 @@ class TimesheetEntryUnitTest {
         
     }
 
-    @Test
-    void associateId_blank() throws Exception {
+    @ParameterizedTest
+    @CsvSource({
+        ",must not be blank",
+        "%$78,must match \"[A-Za-z0-9-]+\"",
+        "ABCDEF-123456-GH,size must be between 1 and 15"
+    })
+    void associateId(String associateId, String message) throws Exception {
         // Given:
         TimesheetEntry entry = validTimesheetEntry();
-        entry.setAssociateId(null);
+        entry.setAssociateId(associateId);
 
         // When:
         Set<ConstraintViolation<TimesheetEntry>> constraintViolations = validator.validate(entry);
@@ -47,46 +54,18 @@ class TimesheetEntryUnitTest {
         assertThat("Validation errors;", constraintViolations, hasSize(1));
         ConstraintViolation<TimesheetEntry> violation = constraintViolations.iterator().next();
         assertThat(violation.getPropertyPath().toString(), is("associateId"));
-        assertThat(violation.getMessage(), is("must not be blank"));
+        assertThat(violation.getMessage(), is(message));
     }
 
-    @Test
-    void associateId_invalidFormat() throws Exception {
+    @ParameterizedTest
+    @CsvSource({
+        "&*(GTRS@$34,must match \"[A-Za-z0-9-]+\"",
+        "ABCDEF-123456-GH,size must be between 1 and 15"
+    })
+    void jobId(String jobId, String message) throws Exception {
         // Given:
         TimesheetEntry entry = validTimesheetEntry();
-        entry.setAssociateId("%$78");
-
-        // When:
-        Set<ConstraintViolation<TimesheetEntry>> constraintViolations = validator.validate(entry);
-
-        // Then:
-        assertThat("Validation errors;", constraintViolations, hasSize(1));
-        ConstraintViolation<TimesheetEntry> violation = constraintViolations.iterator().next();
-        assertThat(violation.getPropertyPath().toString(), is("associateId"));
-        assertThat(violation.getMessage(), is("must match \"[A-Za-z0-9-]+\""));
-    }
-
-    @Test
-    void associateId_tooLong() throws Exception {
-        // Given:
-        TimesheetEntry entry = validTimesheetEntry();
-        entry.setAssociateId("ABCDEF-123456-GH");
-
-        // When:
-        Set<ConstraintViolation<TimesheetEntry>> constraintViolations = validator.validate(entry);
-
-        // Then:
-        assertThat("Validation errors;", constraintViolations, hasSize(1));
-        ConstraintViolation<TimesheetEntry> violation = constraintViolations.iterator().next();
-        assertThat(violation.getPropertyPath().toString(), is("associateId"));
-        assertThat(violation.getMessage(), is("size must be between 1 and 15"));
-    }
-
-    @Test
-    void jobId_invalidFormat() throws Exception {
-        // Given:
-        TimesheetEntry entry = validTimesheetEntry();
-        entry.setJobId("&*(GTRS@$34");
+        entry.setJobId(jobId);
 
         // When:
         Set<ConstraintViolation<TimesheetEntry>> constraintViolations = validator.validate(entry);
@@ -95,30 +74,19 @@ class TimesheetEntryUnitTest {
         assertThat("Validation errors;", constraintViolations, hasSize(1));
         ConstraintViolation<TimesheetEntry> violation = constraintViolations.iterator().next();
         assertThat(violation.getPropertyPath().toString(), is("jobId"));
-        assertThat(violation.getMessage(), is("must match \"[A-Za-z0-9-]+\""));
+        assertThat(violation.getMessage(), is(message));
     }
 
-    @Test
-    void jobId_tooLong() throws Exception {
+    @ParameterizedTest
+    @CsvSource({
+        ",must not be blank",
+        "GHI*(<>?4568,must match \"[A-Za-z0-9-]+\"",
+        "GHIJKLM-12345689,size must be between 1 and 15"
+    })
+    void serviceItemId(String serviceItemId, String message) throws Exception {
         // Given:
         TimesheetEntry entry = validTimesheetEntry();
-        entry.setJobId("ABCDEF-123456-GH");
-
-        // When:
-        Set<ConstraintViolation<TimesheetEntry>> constraintViolations = validator.validate(entry);
-
-        // Then:
-        assertThat("Validation errors;", constraintViolations, hasSize(1));
-        ConstraintViolation<TimesheetEntry> violation = constraintViolations.iterator().next();
-        assertThat(violation.getPropertyPath().toString(), is("jobId"));
-        assertThat(violation.getMessage(), is("size must be between 1 and 15"));
-    }
-
-    @Test
-    void serviceItemId_blank() throws Exception {
-        // Given:
-        TimesheetEntry entry = validTimesheetEntry();
-        entry.setServiceItemId(null);
+        entry.setServiceItemId(serviceItemId);
 
         // When:
         Set<ConstraintViolation<TimesheetEntry>> constraintViolations = validator.validate(entry);
@@ -127,39 +95,7 @@ class TimesheetEntryUnitTest {
         assertThat("Validation errors;", constraintViolations, hasSize(1));
         ConstraintViolation<TimesheetEntry> violation = constraintViolations.iterator().next();
         assertThat(violation.getPropertyPath().toString(), is("serviceItemId"));
-        assertThat(violation.getMessage(), is("must not be blank"));
-    }
-
-    @Test
-    void serviceItemId_invalidFormat() throws Exception {
-        // Given:
-        TimesheetEntry entry = validTimesheetEntry();
-        entry.setServiceItemId("GHI*(<>?4568");
-
-        // When:
-        Set<ConstraintViolation<TimesheetEntry>> constraintViolations = validator.validate(entry);
-
-        // Then:
-        assertThat("Validation errors;", constraintViolations, hasSize(1));
-        ConstraintViolation<TimesheetEntry> violation = constraintViolations.iterator().next();
-        assertThat(violation.getPropertyPath().toString(), is("serviceItemId"));
-        assertThat(violation.getMessage(), is("must match \"[A-Za-z0-9-]+\""));
-    }
-
-    @Test
-    void serviceItemId_tooLong() throws Exception {
-        // Given:
-        TimesheetEntry entry = validTimesheetEntry();
-        entry.setServiceItemId("GHIJKLM-12345689");
-
-        // When:
-        Set<ConstraintViolation<TimesheetEntry>> constraintViolations = validator.validate(entry);
-
-        // Then:
-        assertThat("Validation errors;", constraintViolations, hasSize(1));
-        ConstraintViolation<TimesheetEntry> violation = constraintViolations.iterator().next();
-        assertThat(violation.getPropertyPath().toString(), is("serviceItemId"));
-        assertThat(violation.getMessage(), is("size must be between 1 and 15"));
+        assertThat(violation.getMessage(), is(message));
     }
 
     private TimesheetEntry validTimesheetEntry() {
