338315f25cf78b1fc2e89b18bb941858a95c7448
Brian Schalme <bschalme@airspeed.ca>
2025-05-18 12:39:02 -0500

    Bump Micronaut version from 4.3.5 to 4.8.2, and Gradle from 8.7 to 8.14. Other related vitality.

--- src/main/java/ca/airspeed/qbdapi/adapter/in/web/UserPasswordAuthenticationProvider.java
+++ src/main/java/ca/airspeed/qbdapi/adapter/in/web/UserPasswordAuthenticationProvider.java
@@ -1,27 +1,28 @@
 package ca.airspeed.qbdapi.adapter.in.web;
 
-import io.micronaut.context.annotation.Requires;
-import io.micronaut.http.HttpRequest;
-import io.micronaut.security.authentication.Authentication;
-import io.micronaut.security.authentication.AuthenticationProvider;
-import io.micronaut.security.authentication.AuthenticationRequest;
-import io.micronaut.security.authentication.AuthenticationResponse;
-import io.micronaut.security.authentication.ServerAuthentication;
-import io.reactivex.rxjava3.core.BackpressureStrategy;
-import io.reactivex.rxjava3.core.Flowable;
-import jakarta.inject.Singleton;
-import org.reactivestreams.Publisher;
+import static java.util.Arrays.asList;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static java.util.Arrays.asList;
+import org.reactivestreams.Publisher;
+
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.security.authentication.Authentication;
+import io.micronaut.security.authentication.AuthenticationRequest;
+import io.micronaut.security.authentication.AuthenticationResponse;
+import io.micronaut.security.authentication.ServerAuthentication;
+import io.micronaut.security.authentication.provider.HttpRequestReactiveAuthenticationProvider;
+import io.reactivex.rxjava3.core.BackpressureStrategy;
+import io.reactivex.rxjava3.core.Flowable;
+import jakarta.inject.Singleton;
 
 @Singleton
 @Requires(env={"localhost", "test"})
-public class UserPasswordAuthenticationProvider implements AuthenticationProvider<HttpRequest<?>> {
+public class UserPasswordAuthenticationProvider implements HttpRequestReactiveAuthenticationProvider<HttpRequest<?>> {
 
     private Map<String, String> users;
     private Map<String, List<String>> userGroups;
@@ -39,8 +40,8 @@ public class UserPasswordAuthenticationProvider implements AuthenticationProvide
     }
 
     @Override
-    public Publisher<AuthenticationResponse> authenticate(HttpRequest<?> httpRequest,
-                                                          AuthenticationRequest<?, ?> authenticationRequest) {
+    public Publisher<AuthenticationResponse> authenticate(HttpRequest<HttpRequest<?>> httpRequest,
+                                                          AuthenticationRequest<String, String> authenticationRequest) {
         return Flowable.create(emitter -> {
             Authentication userDetails = doAuthenticate(authenticationRequest);
             if (userDetails != null) {
@@ -50,8 +51,8 @@ public class UserPasswordAuthenticationProvider implements AuthenticationProvide
         }, BackpressureStrategy.ERROR);
     }
 
-    private ServerAuthentication doAuthenticate(AuthenticationRequest<?, ?> authenticationRequest) {
-        String identity = (String) authenticationRequest.getIdentity();
+    private ServerAuthentication doAuthenticate(AuthenticationRequest<String, String> authenticationRequest) {
+        String identity = authenticationRequest.getIdentity();
         String password = this.users.get(identity); 
         if (password != null && password.equals(authenticationRequest.getSecret())) {
             List<String> groups = this.userGroups.get(identity);
